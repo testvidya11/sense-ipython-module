51ea1dbc144afbf57333f6559d73e8dc7ffcc442
apatil <anand.prabhakar.patil@gmail.com>
2013-09-26 16:27:35 -0700

    Fixed environment passing in launch_workers

--- sense.py
+++ sense.py
@@ -216,7 +216,7 @@ def launch_workers(n, size="small", engine="sense-ipython-engine", startup_scrip
     # The n launch requests are done concurrently in a thread pool for lower                                                 
     # latency.                                                                                                               
     def launch_worker(i):
-        out = requests.post(url, data=simplejson.dumps(request_body), auth=(auth["user"], auth["password"]), headers=headers)
+        return requests.post(url, data=simplejson.dumps(request_body), auth=(auth["user"], auth["password"]), headers=headers)
     pool = futures.ThreadPoolExecutor(THREAD_POOL_SIZE)
     responses = [pool.submit(launch_worker, i) for i in xrange(n)]
     return map(lambda x: x.result(), futures.wait(responses)[0])
